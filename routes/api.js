4fa51ea84c93d10a255e888639fdd28aec15ac03
Paul N Barnes <paulieb99@gmail.com>
2021-09-09 20:17:37 -0700

    successful creation of getting specific word meaning

--- routes/api.js
+++ routes/api.js
@@ -10,4 +10,14 @@ router.get('/words', async(req,res) => {
     }
 })
 
+router.get('/words/:word', async(req,res) => {
+    try {
+        const word = req.params.word
+        const wordData = await Dictionary.findOne({ word: word})
+        res.json(wordData.meaning)
+    } catch (error) {
+        throw error
+    }
+})
+
 module.exports = router;
