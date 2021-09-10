1ae51d32cdfc7c020224ec79d4700dd338ea5605
Paul N Barnes <paulieb99@gmail.com>
2021-09-09 17:07:14 -0700

    started server successfully

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dictionary_db", { 
+    useNewUrlParser: true
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
